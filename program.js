4c556a88da61191e09f62a7b3ebd75bce59273b7
Marcin Polonis <marcin.polonis@o2.pl>
2019-02-22 18:36:14 +0100

    Add program.js file

---
+++ program.js
@@ -0,0 +1,23 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function () {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if(input !== null) {
+        var instruction = input.toString().trim();
+        switch(instruction) {
+            case '/version': 
+                console.log('The version of the Node.js you are using now is ' + process.versions.node);
+                break;
+            case '/language':
+                console.log('The language is ' + process.env.LANG);
+                break;
+            case '/exit':
+                process.stdout.write('Quiting app!\n');
+                process.exit();
+                break;
+            default: 
+                process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
